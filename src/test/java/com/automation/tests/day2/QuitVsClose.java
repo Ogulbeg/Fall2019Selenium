9aba46966792e2203b076fc5e444a43a12204ffb
Ogulbeg <aolya0722@gmail.com>
2020-03-01 17:41:01 -0500

    finding the elements

--- src/test/java/com/automation/tests/day2/QuitVsClose.java
+++ src/test/java/com/automation/tests/day2/QuitVsClose.java
@@ -1,8 +1,23 @@
 package com.automation.tests.day2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class QuitVsClose {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+
+        Thread.sleep(5000);//for demo, wait 3 seconds
+
+       // driver.close(); will close only one window
+        driver.quit();// to close all windows
 
 
     }
