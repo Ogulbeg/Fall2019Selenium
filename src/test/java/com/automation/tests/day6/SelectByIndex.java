622b1f4a4a067b15e7887ae4c2e4c4313b9d18fa
Ogulbeg <aolya0722@gmail.com>
2020-03-08 17:35:56 -0400

    Xpath

--- src/test/java/com/automation/tests/day6/SelectByIndex.java
+++ src/test/java/com/automation/tests/day6/SelectByIndex.java
@@ -4,7 +4,10 @@ import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class SelectByIndex {
 
@@ -15,13 +18,19 @@ public class SelectByIndex {
         BrowserUtils.wait(3);
         Select stateSelect = new Select(driver.findElement(By.id("state")));
 
+        List<WebElement> stateLst = stateSelect.getOptions();
+        int count = 0;
+        for (WebElement eachState: stateLst) {
+            System.out.println(count + " = " + eachState.getText());
+            count++;
+        }
+
         // index starts from 0
         stateSelect.selectByIndex(31);
         BrowserUtils.wait(3);
 
         //select last option
         stateSelect.selectByIndex(stateSelect.getOptions().size()-1);
-
 
         BrowserUtils.wait(3);
         driver.quit();
